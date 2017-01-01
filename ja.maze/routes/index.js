805df21b70fe22d416b0f56ac019d3d4d9cc35e0
JAHOME <JAHOME@JAHOME-PC>
2017-01-01 21:53:09 +0800

    添加项目文件。

---
+++ ja.maze/routes/index.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/a', function (req, res) {
+  res.render('a', { title: 'Express' });
+});
+
+module.exports = router;
